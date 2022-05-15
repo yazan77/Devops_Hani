49f3072ad1074f7ca1251f8198cb1c39757f9acf
Hani <hani14293@gmail.com>
2022-05-15 17:09:07 +0300

    new fiels website

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!defined( 'ABSPATH' ) )exit('No Such File'); 
-die('No File/directory exist');
-?>
--- js/wpsp-custom-multicls.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function($) {  $('select.multiselect').multiselect({    includeSelectAllOption: true,    nonSelectedText: 'Select Class'  });});
